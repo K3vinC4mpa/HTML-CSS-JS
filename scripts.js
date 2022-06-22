0bec5d214eec4867e037faba23a3a1487f8229bb
Kevin Campa <kevincampa01@gmail.com>
2022-06-22 13:39:51 -0600

    html-css-js project added

---
+++ scripts.js
@@ -0,0 +1,35 @@
+function getValue() {
+    // var color = document.getElementById("mycolor").value;
+    // alert(color);
+    // var date = document.getElementById("mydate").value;
+    // alert(date);
+    // var datetime = document.getElementById("mydatetime").value;
+    // alert(datetime);
+    // var email = document.getElementById("myemail").value;
+    // alert(email);
+
+    // var month = document.getElementById("mymonth").value;
+    // alert(month);
+
+    // var number = document.getElementById("mynumber").value;
+    // alert(number);
+
+    // var number2 = document.getElementById("mynumber2").value;
+    // alert(number2);
+
+    // var search = document.getElementById("mysearch").value;
+    // alert(search);
+
+    // var tel = document.getElementById("myphone").value;
+    // alert(tel);
+
+    // var time = document.getElementById("mytime").value;
+    // alert(time);
+
+    var url  = document.getElementById("myurl").value;
+    alert(url);
+    var week = document.getElementById("myweek").value;
+    alert(week);
+
+}
+document.getElementById("mono").innerHTML = "Gahara is kinda creepy 4sure...";
